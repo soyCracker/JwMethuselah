3d1f1067ac5dbcd4be4a72bb573dcc0ab91bf88d
yu <yuhui.lai@paypaycloud.com>
2019-12-23 17:14:12 +0800

    jw audio loop

---
+++ execute.js
@@ -0,0 +1,18 @@
+console.log("QQQQQ")
+window.onload = function ()
+{
+    var audio = document.getElementById("vjs_video_3_html5_api")
+    audio.setAttribute('loop',true);
+    /*audio.addEventListener('ended', function(e) 
+    {
+        this.currentTime = 0;
+        this.play();
+    }, false);*/
+
+    /*audio.addEventListener("DOMSubtreeModified", function() 
+    {
+        console.log("UUUUUUU")
+    }, false);*/
+}
+
+

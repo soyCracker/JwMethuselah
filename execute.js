bf3057b18659e188772bd73f2f5f90d7170965d9
yu <yuhui.lai@paypaycloud.com>
2020-03-11 12:54:06 +0800

    fix

--- execute.js
+++ execute.js
@@ -1,19 +1,5 @@
 console.log("JwMethuselah start")
 window.onload = function ()
 {
-    //var audio = document.getElementById("vjs_video_3_html5_api")
-    //audio.setAttribute('loop',true);
     $("audio").attr("loop",true);
-    /*audio.addEventListener('ended', function(e) 
-    {
-        this.currentTime = 0;
-        this.play();
-    }, false);*/
-
-    /*audio.addEventListener("DOMSubtreeModified", function() 
-    {
-        console.log("UUUUUUU")
-    }, false);*/
 }
-
-
